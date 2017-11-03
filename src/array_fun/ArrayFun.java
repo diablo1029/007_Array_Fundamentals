a7fb1362631d561229e8d619161b99546963ff4c
compsci <compsci@compsci>
2017-11-03 10:24:57 -0600

    11/3/2017

--- src/array_fun/ArrayFun.java
+++ src/array_fun/ArrayFun.java
@@ -1,5 +1,42 @@
 package array_fun;
 
+import javax.swing.JOptionPane;
+
 public class ArrayFun {
+
+	public void showInstructions() {
+		
+		String[] msgArray = { 
+				"Essential rules for arrays", 
+				"1. Arrays have a FIXED length!! At the time they are created.",
+				"2. Two ways to set array size:",
+				"-- one way is to set size when constructed",
+				"-- other way is to set size from initial data \\",
+				"Dynamic - when program is running:",
+				"But OF COURSE you can change values!",
+				"Can NOT NO WAY NO HOW change the SIZE of the array!"
+				};
+		JOptionPane.showMessageDialog(null,  msgArray);
+	}
+	public void intArrayExample() {
+		
+		int[] intTest = new int[10];
+		intTest[0] = 10;
+		intTest[1] = 11;
+		intTest[2] = 12;
+		intTest[3] = 13;
+		intTest[4] = 14;
+		intTest[5] = 15;
+		intTest[6] = 16;
+		intTest[7] = 17;
+		intTest[8] = 18;
+		intTest[9] = 19;
+		int[] intTwoTest = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+		System.out.println("Strarting test of intTest[]\n");
+		for (int i = 0; i < intTest.length; i++) {
+			System.out.println("intTest[" + i + "] " + intTest[i]);
+		}
+	
+	}
 
 }
--- src/array_fun/TheMainClass.java
+++ src/array_fun/TheMainClass.java
@@ -3,6 +3,12 @@ package array_fun;
 public class TheMainClass {
 
 		public static void main(String[] args) {
+
+			ArrayFun learnArrays = new ArrayFun();
+			learnArrays.showInstructions();
+			learnArrays.intArrayExample();
+			
 			
 		}
+		
 }
